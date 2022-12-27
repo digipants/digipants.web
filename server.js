a44a278d42fd4a91463be2c04b6ea3e761254005
Bhupendra Kumar <bhupendramishra216@gmail.com>
2022-12-27 18:43:08 +0530

    pages rendered

--- server.js
+++ server.js
@@ -2,20 +2,42 @@ const express = require('express')
 const app = express()
 
 // static files
-app.use(express.static(__dirname + '/DigiPants'))
-app.use(express.static(__dirname + '/DigiPants/css'))
-app.use(express.static(__dirname + '/DigiPants/63238e717124a8c9a4c94e41'))
-app.use(express.static(__dirname + '/DigiPants/js'))
-app.use(express.static(__dirname + '/DigiPants/632622573bb4e8a1d6504d34'))
+app.use(express.static(__dirname + '/public'));
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.html')
+})
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/public/about-us.html');
+})
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/public/about-us.html');
+})
+app.get('/services', (req, res) => {
+    res.sendFile(__dirname + '/public/services.html');
+})
+app.get('/works', (req, res) => {
+    res.sendFile(__dirname + '/public/works.html');
+})
+app.get('/pricing', (req, res) => {
+    res.sendFile(__dirname + '/public/pricing.html');
+})
+app.get('/faq', (req, res) => {
+    res.sendFile(__dirname + '/public/faq.html');
+})
+app.get('/blog', (req, res) => {
+    res.sendFile(__dirname + '/public/blog.html');
+})
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname + '/public/contact-us.html');
 })
 
+
 app.get('/login', (req, res) => {
-    res.render('login.ejs')
+    res.render('login-page.ejs')
 })
 
 app.get('/register', (req, res) => {
@@ -23,11 +45,12 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    res.render('register.ejs')
+    res.render('login.ejs')
 })
 
 app.post('/register', (req, res) => {
     res.render('register.ejs')
 })
+
 
 app.listen(3000)
