141d1fd819c6f6a72b73a3d0f6fce8232ea462d5
Bhupendra Kumar <bhupendra@digipants.com>
2023-03-02 21:36:33 +0530

    login

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,6 @@
-const { NetlifyIdentity } = require('netlify-identity-widget');
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/assets');
-
-    eleventyConfig.addNunjucksAsyncShortcode('auth', async function () {
-        const user = NetlifyIdentity.currentUser();
-        if (!user) {
-            return `<a href="/login-page">Log in to view pricing</a>`;
-        }
-        return `<p>Here is the pricing information...</p>`;
-    });
 
     return {
         dir: {
