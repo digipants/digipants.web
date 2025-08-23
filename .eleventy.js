70cac4ba92e459f3a2a6a3b14004e18ba437e1c9
Bhupendra Kumar <bhupendra@digipants.com>
2025-08-23 18:21:30 +0530

    auto

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/robots.txt');
     eleventyConfig.addPassthroughCopy('./src/sitemap.xml');
-    eleventyConfig.addPassthroughCopy('./src/netlify.toml');
     eleventyConfig.addPassthroughCopy('./src/_redirects');
     
     return {
